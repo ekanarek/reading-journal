7667a262ce77b428d3916ef487ccedc033fa8acd
Emily Kanarek <emilyakanarek@gmail.com>
2025-07-01 10:37:20 -0400

    Capture search results from Open Library API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
   const handleSearch = (search) => {
-    console.log(search);
+    fetch(`https://openlibrary.org/search.json?q=${search}&limit=20`)
+      .then((r) => r.json())
+      .then((data) => {
+        const books = data.docs.map((book) => {
+          const OLID = book.cover_edition_key;
+          if (OLID && book.title && book.author_name) {
+            const bookCover = `https://covers.openlibrary.org/b/olid/${OLID}-M.jpg`;
+            const bookObj = {
+              title: book.title,
+              author: book.author_name[0],
+              image: bookCover,
+            };
+            return bookObj;
+          }
+        });
+        setSearchResults(books.filter((book) => book));
+      });
   };
 
   return (
