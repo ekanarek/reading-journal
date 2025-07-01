ca8cdf445e56539d1701808b74ea415329dc3cf7
Emily Kanarek <emilyakanarek@gmail.com>
2025-07-01 11:55:04 -0400

    Populate book data in form

--- src/pages/AddForm.js
+++ src/pages/AddForm.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 const AddForm = () => {
   const { selectedBook } = useOutletContext();
   console.log(selectedBook);
 
+  const [image, setImage] = useState(selectedBook.image);
+
   return (
     <div>
-      <p>Form here</p>
+      <form>
+        <h3>Add A New Journal Entry</h3>
+        <label>Title: </label>
+        <input type="text" value={selectedBook.title} readOnly="readOnly" />
+        <label>Author: </label>
+        <input type="text" value={selectedBook.author} readOnly="readOnly" />
+        <label>Cover Image URL: </label>
+        <input
+          type="text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
