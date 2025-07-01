bfc85bf470505693a30b06ddad839a934544b31b
Emily Kanarek <emilyakanarek@gmail.com>
2025-07-01 08:54:59 -0400

    Click card to view notes

--- src/components/JournalCard.js
+++ src/components/JournalCard.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const JournalCard = ({ entry }) => {
   const { title, author, image, started, finished, rating, notes } = entry;
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <div className="journalCard">
-      <img alt={title} src={image} />
-      <div>
-        <p>Title: {title}</p>
-        <p>Author: {author}</p>
-        <p>Date Started: {started}</p>
-        <p>Date Finished: {finished}</p>
-        <p>Rating: {rating}/5</p>
-        <p>Click to see more</p>
-      </div>
+    <div className="journalCard" onClick={() => setIsClicked(!isClicked)}>
+      {isClicked ? (
+        <p>{notes}</p>
+      ) : (
+        <>
+          <img alt={title} src={image} />
+          <div>
+            <p>Title: {title}</p>
+            <p>Author: {author}</p>
+            <p>Date Started: {started}</p>
+            <p>Date Finished: {finished}</p>
+            <p>Rating: {rating}/5</p>
+            <p>Click to see more</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
