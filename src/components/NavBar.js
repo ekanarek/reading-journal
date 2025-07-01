90fad00e98b4dfbe70e716c465e187d161a196f9
Emily Kanarek <emilyakanarek@gmail.com>
2025-07-01 09:38:16 -0400

    Add home link and search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import journalIcon from "../icon.png";
 
 const NavBar = () => {
-  return <nav></nav>;
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/search");
+  };
+
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">
+          <img src={journalIcon} id="journalIcon" />
+        </NavLink>
+      </nav>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search for a book..." />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
 };
 
 export default NavBar;
